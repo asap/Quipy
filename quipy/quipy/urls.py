9c1503b285adf1aa0a8db46c460a5097fa9ddfad
Alexander Sapountzis <asap@briteideas.com>
2012-06-12 21:19:20 -0400

    Simplifying project because I forgot to commit the first version

--- quipy/quipy/urls.py
+++ quipy/quipy/urls.py
@@ -3,18 +3,18 @@ from django.contrib import admin
 
 from tastypie.api import Api
 
-from quote.api import QuoteResource, UserResource
+from quote.api import QuoteResource #, UserResource
 
 admin.autodiscover()
 
-# quote_resource = QuoteResource()
+quote_resource = QuoteResource()
 
-v1_api = Api(api_name='v1')
-v1_api.register(UserResource())
-v1_api.register(QuoteResource())
+# v1_api = Api(api_name='v1')
+# v1_api.register(UserResource())
+# v1_api.register(QuoteResource())
 
 urlpatterns = patterns('',
     url(r'^quotes/', include('quote.urls')),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^api/', include(v1_api.urls)),
+    url(r'^api/', include(quote_resource.urls)),
 )
--- quipy/quote/api.py
+++ quipy/quote/api.py
@@ -7,34 +7,7 @@ from tastypie.utils import trailing_slash
 from tastypie import fields
 from quote.models import Quote
 
-class UserResource(ModelResource):
-    class Meta:
-        queryset = User.objects.all()
-        resource_name = 'user'
-        fields = ['username', 'first_name', 'last_name', 'last_login', 'email']
-        allowed_methods = ['get']
-
 class QuoteResource(ModelResource):
-    user = fields.ForeignKey(UserResource, 'user')
 
     class Meta:
         queryset = Quote.objects.all()
-        authorization = Authorization()
-
-    def override_urls(self):
-        return [
-            url(r"^(?P<resource_name>%s)/random%s$" % (self._meta.resource_name, trailing_slash()), self.wrap_view('get_random'), name="api_get_random"),
-        ]
-
-    def get_random(self, request, **kwargs):
-        self.method_check(request, allowed=['get'])
-        self.is_authenticated(request)
-        self.throttle_check(request);
-
-        # Do the query.
-        try:
-            q = Quote.objects.order_by('?')[0]
-        except Quote.DoesNotExist:
-            raise Http404
-        self.log_throttled_access(request)
-        return self.create_response(request, q)
